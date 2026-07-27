f7dd35bcf77b12ee24b41dc35bee02a1acb00400
jean paul garcia gutierrez <garciajeanp6@gmail.com>
2026-07-26 20:22:52 -0500

    trecer comit el primer ejemplo de el ciclo do-while

---
+++ do-while/src/Ejemplo1.java
@@ -0,0 +1,10 @@
+
+public class Ejemplo1 {
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println("Número: " + i);
+            i++;
+        } while (i <= 5);
+    }
+}
