b430ddc564b2241dae72e374be00134849cbeea3
jean paul garcia gutierrez <garciajeanp6@gmail.com>
2026-07-26 20:49:48 -0500

    Agrego y actualizo Ejemplo2 y Ejemplo3

---
+++ do-while/src/Ejemplo2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Ejemplo2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numero;
+        do {
+            System.out.print("Ingrese un número entre 1 y 10: ");
+            numero = sc.nextInt();
+        } while (numero < 1 || numero > 10);
+        System.out.println("Número válido: " + numero);
+    }
+}
+
---
+++ do-while/src/Ejemplo3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Ejemplo3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int opcion;
+        do {
+            System.out.println("=== MENÚ ===");
+            System.out.println("1. Sumar");
+            System.out.println("2. Restar");
+            System.out.println("0. Salir");
+            System.out.print("Opción: ");
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1 -> System.out.println("Has elegido sumar.");
+                case 2 -> System.out.println("Has elegido restar.");
+                case 0 -> System.out.println("Saliendo...");
+                default -> System.out.println("Opción inválida.");
+            }
+        } while (opcion != 0);
+    }
+}
